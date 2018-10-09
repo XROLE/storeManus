1dbd11ab5bcbba3f23af10aa0c1d044ec6ebab18
XROLE <xrolediamond@gmail.com>
2018-10-09 13:54:46 -0700

    handled displaying dashboard functionality when user click on signup button

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ window.onload = (() => {
    const hanburgerButton = document.querySelector('.hanburgerButton')  // get hanburger Button
    const close = document.querySelector('.close')  // get close Button
    const navItems = document.querySelector('.nav-items'); //get nav items
+   const signupButton = document.querySelector('.signup-button') // get signup button
+   const signinButton =document.querySelector('.signin-button') // get signin button
 
    hanburgerButton.addEventListener('click', () => {
        hanburgerButton.className = 'hide';
@@ -14,4 +16,15 @@ window.onload = (() => {
        hanburgerButton.className ='fa fa-bars hanburgerButton';
     navItems.className = 'hide';
    })
+
+   signupButton.addEventListener('click', (signupEvent) => {
+       signupEvent.preventDefault();
+       window.location.href = 'file:///C:/Users/XROLE%20VALSIDO%20DIAMON/Desktop/Apps/storeManus/views/adminDashboard.html';
+   })
+
+   signinButton.addEventListener('click', (signinEvent) => {
+       signinEvent.preventDefault();
+       alert('I am a chosen one');
+       window.location.href = 'file:///C:/Users/XROLE%20VALSIDO%20DIAMON/Desktop/Apps/storeManus/views/adminDashboard.html';
+       })
 })
