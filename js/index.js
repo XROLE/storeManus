81ff0960ea725bf8adc7cde3806d167eb341e8a1
XROLE <xrolediamond@gmail.com>
2018-10-09 19:29:18 -0700

    handled mobile responsiveness of dashboard

--- js/index.js
+++ js/index.js
@@ -4,6 +4,11 @@ window.onload = (() => {
    const navItems = document.querySelector('.nav-items'); //get nav items
    const signupButton = document.querySelector('.signup-button') // get signup button
    const signinButton =document.querySelector('.signin-button') // get signin button
+   const displayDasboardContents = document.querySelector('.dasboard-content-toggle-show') // get toggler button
+   const dasboardContentDiv = document.querySelector('.dasboard-content') // get dashboard content div
+   const closeToggle = document.querySelector('.close-toggle') // get close toggle button
+   const dasboardContentMain = document.querySelector('.dasboard-content-main') // get main content div
+
 
    hanburgerButton.addEventListener('click', () => {
        hanburgerButton.className = 'hide';
@@ -17,6 +22,19 @@ window.onload = (() => {
     navItems.className = 'hide';
    })
 
+    displayDasboardContents.addEventListener('click', () => {
+        dasboardContentDiv.className = 'dasboard-content-increase-with';
+        dasboardContentMain.className = 'dasboard-content-main-show';
+        displayDasboardContents.className = 'hide';
+        closeToggle.className = 'fa fa-times';
+    })
+    closeToggle.addEventListener('click', () => {
+        closeToggle.className ='hide';
+        dasboardContentDiv.className ='dasboard-content';
+        dasboardContentMain.className = 'hide';
+        displayDasboardContents.className ='fa fa-bars dasboard-content-toggle-show';
+    })
+
    signupButton.addEventListener('click', (signupEvent) => {
        signupEvent.preventDefault();
        window.location.href = 'file:///C:/Users/XROLE%20VALSIDO%20DIAMON/Desktop/Apps/storeManus/views/adminDashboard.html';
@@ -26,5 +44,7 @@ window.onload = (() => {
        signinEvent.preventDefault();
        alert('I am a chosen one');
        window.location.href = 'file:///C:/Users/XROLE%20VALSIDO%20DIAMON/Desktop/Apps/storeManus/views/adminDashboard.html';
-       })
+    })
+
+    
 })
