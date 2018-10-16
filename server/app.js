e0ba967acafcc8b4e4cd03dae97315bedd8c292e
XROLE <xrolediamond@gmail.com>
2018-10-16 14:55:59 -0700

    added the missing '.' in front of the eslintrc.json file

--- server/app.js
+++ server/app.js
@@ -3,11 +3,12 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hi, you have succesfully installed expres js');
-})
+});
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log('Server started and runing on port 5000.....')
+    console.log('Server started and runing on port 5000.....');
 });
+
 
