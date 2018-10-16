30a56a41f94ac04decf05b989e9ca15bc57cb8fa
XROLE <xrolediamond@gmail.com>
2018-10-16 12:45:52 -0700

    installed express js

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi, you have succesfully installed expres js');
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log('Server started and runing on port 5000.....')
+});
