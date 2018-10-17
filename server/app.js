9104d4ef3e68173717b5d0dad21b472eefafa530
XROLE <xrolediamond@gmail.com>
2018-10-16 19:16:31 -0700

    installed and configured body-parser

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import router from '../routes/route';
 
+
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json);
+
 app.use('/', router);
+
+
 
 const port = process.env.PORT || 5000;
 
