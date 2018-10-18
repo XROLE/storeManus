b767c479244e9db5e0a901665f28b834ef94067c
XROLE <xrolediamond@gmail.com>
2018-10-18 07:59:28 -0700

    Done setting up test environment

---
+++ test/test.js
@@ -0,0 +1,12 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { products } from '../model/data';
+import { sales } from '../model/data';
+import server from '../server/app';
+
+
+chai.use(chaiHttp);
+let expect = chai.expect;
+    describe('Store Manus test', () => {
+        beforeEach(done)
+    })
