d3ae8648fa0eeb8ad9a53b0b3baa518a4b554d64
XROLE <xrolediamond@gmail.com>
2018-10-26 05:49:58 -0700

    CH(still refactoring validate.js)

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,22 +1,4 @@
-// export  default class validate {
 
-//     static isString(value){
-//         return typeof value === 'string';
-//     }
-//     static isNumber(number) {
-//         return !isNaN(number);
-//     }
-//     static isValidNum(number) {
-//         return number.length === 13;
-//     }
-//     static isEmpty(value) {
-//         return (typeof value === 'undefined' || value.trim === '' || value.length === 0);
-//     }
-// }
-
-export const  isString = (value) =>{
-    return typeof value === 'string';
-};
 export const isNumber = (number) => {
     return !isNaN(number);
 };
