6d320e554f0ba95cfb0416f98ac848429b8b842c
XROLE <xrolediamond@gmail.com>
2018-10-26 11:01:51 -0700

    CH(create test for unit validation functions)

--- test/test.js
+++ test/test.js
@@ -3,7 +3,11 @@ import chaiHttp from 'chai-http';
 import { products } from '../model/data';
 import { sales } from '../model/data';
 import server from '../server/app';
+import { isNumber } from '../middlewares/validate';
+import { isValidNum } from '../middlewares/validate';
+import { isEmpty } from '../middlewares/validate';
 
+var assert = require('assert');
 
 chai.use(chaiHttp);
 let expect = chai.expect;
@@ -193,6 +197,18 @@ describe('STOREMANUS', () => {  // ====================================== Empty 
     });
 });
 
+// Functional test for validation
 
+describe('Test Validation functions', function () {
+ it('Should return true when a number is provided', function () {
+        assert.equal( isNumber(5), true);
+    });
+ it('Should return false if the length of the number is not equal to 13', function () {
+        assert.equal( isValidNum(9999999999999), false);
+    });
+ it('Should return false if the input is not empty', function () {
+        assert.equal( isEmpty(8), false);
+    });
+});
 
 
