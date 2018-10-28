8cbb4d89ed5e499fbb92015fcdd41463842c914a
XROLE <xrolediamond@gmail.com>
2018-10-28 05:56:38 -0700

    test bcrypt hashing with promise

--- server/app.js
+++ server/app.js
@@ -2,16 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import router from '../routes/route';
-dotenv.config();
+import bcrypt from 'bcrypt';
 
 
+dotenv.config();  // configuring environment variable
+const saltRounds = 10;
+
+// Initial express app
 const app = express();
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 app.use('/', router);
+
+bcrypt.hash('IamachosenONE', saltRounds).then((hash) => {
+    console.log(hash);
+});
 
 const port = process.env.PORT || 5000;
 
