bf16f6710d1ac943ca9c160d6489849b2cfb10b9
XROLE <xrolediamond@gmail.com>
2018-10-31 23:35:13 -0700

    adding test

--- server/app.js
+++ server/app.js
@@ -2,23 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import router from '../routes/route';
-import bcrypt from 'bcrypt';
+
 
 
 dotenv.config();  // configuring environment variable
-const saltRounds = 10;
 
 // Initial express app
 const app = express();
 
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/', router);
-
-bcrypt.hash('IamachosenONE', saltRounds).then((hash) => {
-    console.log(hash);
-});
 
 const port = process.env.PORT || 5000;
 
